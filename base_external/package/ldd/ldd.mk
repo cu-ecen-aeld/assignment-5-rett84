1d61678b85918137dbb3d80785c55fa1c7a9b823
rett84 <lucasrettore31@hotmail.com>
2025-04-23 11:58:07 -0400

    Updated assignment 7

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-LDD_VERSION = d79ef7f6c084acaad6e3ed45a03b5ce103e46e87
+LDD_VERSION = 05a2b981889fcbd60a02ee6fd2d0528dea96f86f
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,9 +23,8 @@ define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
-	
-	$(INSTALL) -m 0755 $(@D)/S98lddmodules $(TARGET_DIR)/etc/init.d/S98lddmodules
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/	
+	#$(INSTALL) -m 0755 $(@D)/misc-modules/S98lddmodules $(TARGET_DIR)/etc/init.d/S98lddmodules
 endef
 LDD_POST_BUILD_HOOKS += LDD_INSTALL_TARGET_CMDS
 
